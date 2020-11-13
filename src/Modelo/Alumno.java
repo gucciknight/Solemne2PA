95fdd5ee200f278428d5dff384529022a43a0bda
Emilio Silva <emiliosilva.briones@gmail.com>
2020-11-13 19:55:40 -0300

    Alumno y Admin
    Se crean clases Alumno y Admin con los parametros que se guardaran en la base de datos

---
+++ src/Modelo/Alumno.java
@@ -0,0 +1,58 @@
+package Modelo;
+
+public class Alumno {
+    int idAlumno;
+    String loginAlumno;
+    String claveAlumno;
+    String nombreAlumno;
+    String apellidoAlumno;
+    int nivelAlumno;
+
+    public int getIdAlumno() {
+        return idAlumno;
+    }
+
+    public void setIdAlumno(int idAlumno) {
+        this.idAlumno = idAlumno;
+    }
+
+    public String getLoginAlumno() {
+        return loginAlumno;
+    }
+
+    public void setLoginAlumno(String loginAlumno) {
+        this.loginAlumno = loginAlumno;
+    }
+
+    public String getClaveAlumno() {
+        return claveAlumno;
+    }
+
+    public void setClaveAlumno(String claveAlumno) {
+        this.claveAlumno = claveAlumno;
+    }
+
+    public String getNombreAlumno() {
+        return nombreAlumno;
+    }
+
+    public void setNombreAlumno(String nombreAlumno) {
+        this.nombreAlumno = nombreAlumno;
+    }
+
+    public String getApellidoAlumno() {
+        return apellidoAlumno;
+    }
+
+    public void setApellidoAlumno(String apellidoAlumno) {
+        this.apellidoAlumno = apellidoAlumno;
+    }
+
+    public int getNivelAlumno() {
+        return nivelAlumno;
+    }
+
+    public void setNivelAlumno(int nivelAlumno) {
+        this.nivelAlumno = nivelAlumno;
+    }   
+}
