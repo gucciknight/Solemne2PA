a9b9a40afb109f90982e7018b59341e38b4c4473
gucciknight <arodriguezl3>
2020-11-17 01:10:45 -0300

    Update OperacionesAlumno.java

--- src/Modelo/OperacionesAlumno.java
+++ src/Modelo/OperacionesAlumno.java
@@ -4,26 +4,31 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import Vista.Listado;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import net.proteanit.sql.DbUtils;
 
 
 public class OperacionesAlumno extends Conexion {
-    private int rs;
+     public ResultSet rs;
+     private Statement st;
+     private Connection con;
     
-    public void ListarAlumnos(Alumno eq ) {
+    public void ListarAlumnos(Alumno al ) {
         PreparedStatement ps = null;
-        ResultSet rs = null;
         Connection con = getConexion();
+
+        String sql = "select `nombre`, `apellidos` from `alumno` where nivel_id =" + al.getIdNivelAlumno() +"";
+
         try{
-           // String sql = "select `nombre`, `apellido` from `alumno` where id = "+ eq.nivelAlumno +"";
-           // rs = ps.executeQuery(sql);
-            System.out.println("Data from online Database :");
-            while(rs.next()){
-                String nombre = rs.getString("nombre");
-                String apellido = rs.getString("marca");
-
-
-                System.out.println("nombre :"+ nombre +" "+"apellido :"+ apellido +"");
-            }
+           ps = con.prepareStatement(sql);
+           rs = ps.executeQuery(sql);
+           Listado listado = new Listado();
+           listado.setVisible(true);
+           listado.fetch(rs);
             
         }catch(Exception ex){
             System.out.println("Error is found :"+ex);
